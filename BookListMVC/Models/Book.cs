f0ed7f66a1229b539d89e1eecfd80e5d0c8218ee
fringe225 <turalka@code.edu.az>
2022-04-16 17:00:26 +0400

    Add project files.

---
+++ BookListMVC/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookListMVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+    }
+}
